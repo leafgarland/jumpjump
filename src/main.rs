718e067394f547ebfa518293868b8c1c099d8998
Leaf Garland <leafgarland@users.noreply.github.com>
2018-07-10 17:50:58 +1200

    In mem db for testing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate failure;
 extern crate url;
 
 use failure::Error;
-use rusqlite::Connection;
+use rusqlite::{Connection, version};
 use std::path::{Path, PathBuf};
 use url::Url;
 
@@ -22,24 +22,31 @@ fn get_database_path() -> Result<PathBuf, Error> {
 }
 
 fn ensure_tables(dbc: &Connection) -> Result<(), Error> {
-    dbc.execute("create table if not exists jump_location (id INTEGER PRIMARY KEY ASC, location STRING UNIQUE)", &[])?;
+    dbc.execute("create table if not exists jump_location (id INTEGER PRIMARY KEY ASC, location STRING UNIQUE, rank INTEGER);
+                 create index if not exists location_index on jump_location(location)", &[])?;
     Ok(())
 }
 
-fn canonicalize_path<P: AsRef<Path>>(path: P) -> Result<PathBuf, Error> {
+fn canonicalize_path<P: AsRef<Path>>(path: P) -> Result<String, Error> {
     let canonical = path.as_ref().canonicalize()?;
     if cfg!(target_os = "windows") {
         let url = Url::from_file_path(&canonical).map_err(|_| format_err!("Failed to build url"))?;
         let path = url.to_file_path()
             .map_err(|_| format_err!("Failed to build url"))?;
-        return Ok(path);
+        let cow = path.to_string_lossy();
+        let fs = cow.replace('\\', "/");
+        return Ok(fs);
     }
-    Ok(canonical)
+    Ok(canonical.to_string_lossy().to_string())
 }
 
 impl Database {
-    pub fn new() -> Result<Database, Error> {
-        let connection = Connection::open(get_database_path()?)?;
+    pub fn new(path: Option<PathBuf>) -> Result<Database, Error> {
+        let connection = match path {
+            Some(path) => Connection::open(path)?,
+            None => Connection::open_in_memory()?,
+        };
+        println!("db version is {}", version());
         ensure_tables(&connection)?;
         Ok(Database { connection })
     }
@@ -52,17 +59,26 @@ impl Database {
     pub fn add_location<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
         let a_path = canonicalize_path(path.as_ref())?;
         self.connection.execute(
-            "insert or ignore into jump_location(location) values(?)",
-            &[&a_path.to_str().unwrap()],
+            // can use this with sqlite 3.24+
+            // "insert into jump_location(location, rank) values(?, 1) on conflict(location) do update set rank=rank+1",
+            "with new(location) as (values(?)) insert or replace into jump_location(id, location, rank)
+             select old.id, new.location, (coalesce(old.rank + 1, 1)) as rank from new left join jump_location old on old.location = new.location",
+            &[&a_path]
         )?;
         Ok(())
+    }
+
+    pub fn get_locations(&self) -> &[&str] {
+        let mut stmt = self.connection.prepare("select * from jump_location").unwrap();
     }
 }
 
 fn main() -> Result<(), Error> {
-    let db = Database::new()?;
+    let db = Database::new(Option::None)?;
 
     db.clear()?;
+    db.add_location("/dev/tools")?;
+    db.add_location("/dev/tools")?;
     db.add_location("/dev/tools")?;
 
     Ok(())
