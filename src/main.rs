8a3b0fbaf5a7de5aedd2c08cd6f8ddb6b0202837
Leaf Garland <leafgarland@users.noreply.github.com>
2018-08-06 16:13:48 +1200

    spike out paths

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate rusqlite;
 #[macro_use(format_err)]
 extern crate failure;
+extern crate itertools;
 extern crate url;
+extern crate path_abs;
 
 use failure::Error;
-use rusqlite::{Connection, version};
+use rusqlite::{version, Connection};
+use std::env;
 use std::path::{Path, PathBuf};
+use path_abs::PathArc;
 use url::Url;
+
+use itertools::join;
 
 struct Database {
     connection: Connection,
@@ -28,10 +34,12 @@ fn ensure_tables(dbc: &Connection) -> Result<(), Error> {
 }
 
 fn canonicalize_path<P: AsRef<Path>>(path: P) -> Result<String, Error> {
-    let canonical = path.as_ref().canonicalize()?;
+    // let canonical = path.as_ref().canonicalize()?;
+    let canonical = PathArc::new(path.as_ref()).absolute()?;
     if cfg!(target_os = "windows") {
         let url = Url::from_file_path(&canonical).map_err(|_| format_err!("Failed to build url"))?;
-        let path = url.to_file_path()
+        let path = url
+            .to_file_path()
             .map_err(|_| format_err!("Failed to build url"))?;
         let cow = path.to_string_lossy();
         let fs = cow.replace('\\', "/");
@@ -51,10 +59,10 @@ impl Database {
         Ok(Database { connection })
     }
 
-    pub fn clear(&self) -> Result<(), Error> {
-        self.connection.execute("delete from jump_location", &[])?;
-        Ok(())
-    }
+    // pub fn clear(&self) -> Result<(), Error> {
+    //     self.connection.execute("delete from jump_location", &[])?;
+    //     Ok(())
+    // }
 
     pub fn add_location<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
         let a_path = canonicalize_path(path.as_ref())?;
@@ -68,36 +76,106 @@ impl Database {
         Ok(())
     }
 
-    pub fn get_locations(&self) -> &[&str] {
-        let mut stmt = self.connection.prepare("select * from jump_location").unwrap();
+    pub fn get_locations(&self) -> Result<Vec<String>, Error> {
+        let mut stmt = self
+            .connection
+            .prepare("select location from jump_location order by rank desc")?;
+        let results_iter = stmt.query_map(&[], |row| row.get(0))?;
+
+        let mut locations = Vec::new();
+        for r in results_iter {
+            locations.push(r?);
+        }
+
+        Ok(locations)
+    }
+
+    pub fn get_matching_locations(&self, patterns: &[&str]) -> Result<Vec<String>, Error> {
+        let mut pattern = join(patterns, "*");
+        pattern.insert(0, '*');
+        pattern.push('*');
+        let mut stmt = self.connection.prepare_cached(
+            "select location from jump_location where location glob ? order by rank desc",
+        )?;
+        let results_iter = stmt.query_map(&[&pattern], |row| row.get(0))?;
+
+        let mut locations = Vec::new();
+        for r in results_iter {
+            locations.push(r?);
+        }
+
+        Ok(locations)
     }
 }
 
-fn main() -> Result<(), Error> {
-    let db = Database::new(Option::None)?;
-
-    db.clear()?;
-    db.add_location("/dev/tools")?;
-    db.add_location("/dev/tools")?;
-    db.add_location("/dev/tools")?;
-
+fn report_all_locations(db: &Database) -> Result<(), Error> {
+    let locations = db.get_locations()?;
+    for l in locations.iter() {
+        println!("loc: {:?}", l);
+    }
     Ok(())
 }
 
-// fn foo() {
-//     let mut stmt = conn.prepare("SELECT id, name, time_created, data FROM person")?;
-//     let person_iter = stmt.query_map(&[], |row| {
-//         Person {
-//             id: row.get(0),
-//             name: row.get(1),
-//             time_created: row.get(2),
-//             data: row.get(3)
-//         }
-//     })?;
+fn report_best_location(db: &Database, patterns: &[&str]) -> Result<(), Error> {
+    let locations = db.get_matching_locations(patterns)?;
+    if let Some(location) = locations.first() {
+        println!("loc: {:?}", location);
+    }
+    Ok(())
+}
 
-//     for person in person_iter {
-//         println!("Found person {:?}", person?);
-//     }
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
 
-//     Ok(())
-// }
+    let db = Database::new(Option::Some(get_database_path()?))?;
+    match args.iter().map(|s| s.as_ref()).collect::<Vec<&str>>()[1..] {
+        ["add", location] => db.add_location(location)?,
+        ["get"] => report_all_locations(&db)?,
+        ref largs if largs[0] == "get" => report_best_location(&db, &largs[1..])?,
+        _ => println!("Failed to parse arguments"),
+    };
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo() {
+        let db = Database::new(Option::None).unwrap();
+
+        db.add_location("/dev/tools").unwrap();
+        db.add_location("/dev/tools").unwrap();
+        db.add_location("/dev/tools/vim").unwrap();
+        db.add_location("/dev/tools/vim").unwrap();
+        db.add_location("/dev/tools/vim").unwrap();
+
+        let locations: Vec<String> = db.get_locations().unwrap();
+
+        assert_eq!(locations[..], ["C:/dev/tools/vim", "C:/dev/tools"]);
+    }
+
+    #[test]
+    fn get() {
+        let db = Database::new(Option::None).unwrap();
+
+        db.add_location("/dev/tools").unwrap();
+        db.add_location("/dev/tools/vim").unwrap();
+
+        let locations: Vec<String> = db.get_matching_locations(&["vim"]).unwrap();
+
+        assert_eq!(locations[..], ["C:/dev/tools/vim"]);
+    }
+
+    #[test]
+    fn non_existing_path() {
+        let db = Database::new(Option::None).unwrap();
+
+        db.add_location("foo/bar/doolally").unwrap();
+
+        let locations: Vec<String> = db.get_locations().unwrap();
+
+        assert_eq!(locations[..], ["C:/dev/me/jumpjump/foo/bar/doolally"]);
+    }
+}
